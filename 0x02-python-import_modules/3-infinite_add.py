f8c5cba532050ad6f367f8b6a6334771630075bb
Anteneh Anley <you@example.com>
2022-09-28 05:54:08 +0000

    import & modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,23 @@
+mport sys
+
+
+
+if __name__ == '__main__':
+
+        av = sys.argv
+
+            l_av = len(av)
+
+                sum = 0
+
+
+
+                    if l_av > 1:
+
+                                for i in range(1, l_av):
+
+                                                sum += int(av[i])
+
+
+
+                                                    print(sum)
