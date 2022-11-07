2eb644c9b3d15594adaad60a12f0591c1ffd2a4f
Anteneh Anley <anteneh.anley48@gmail.com>
2022-11-07 06:47:12 +0000

    5-print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for num in range(0, 100):
-    if num != 99:
- print("{:02d}, ".format(num), end='')
-    else:
-print("{:02d}".format(num))
+for nums in range(0, 90):
+    if nums == 01 or nums == 09
+        continue
+    print("{:02d}".format(nums), end=", ")
